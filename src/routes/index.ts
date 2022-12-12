3bb3911ab32114018c2e0f929b245ceb7c56656e
akshat-khosya <akshatdps12@gmail.com>
2022-12-12 13:05:51 +0530

    swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Express } from "express";
 import productRoute from "./product.route";
 import userRoute from "./user.route";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../utils/swagger.json';
 
 export default function (app: Express) {
     app.use(userRoute());
     app.use(productRoute());
+    app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 }
